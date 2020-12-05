499fa674278f233a01fea38fa0ab594528f1aeb3
hitchhicker007 <hitchhicker8401@hotmail.com>
2020-12-05 12:34:16 +0530

    'new'

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -22,8 +22,9 @@ function About() {
 
                 <section className="s2">
                     <div className="sub-container">
-                        <h1>Lorem Ipsum</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ullam rerum quidem perferendis repellat eligendi minima adipisci est asperiores delectus. Accusamus quas voluptate illum nemo commodi cum porro amet ratione!</p>
+                        <h1>What is a Sleep Tracker??</h1>
+                        <p>Sleep tracker is a tool made by <a href="http://hitchhicker.tech/">Hitch Hicker</a> and <a href="http://karan-munjani.tech/">Karan Munjani.</a> This tool will help you to track your sleep in a better way. This tool is based on the rule of 4 Stages of Sleep. By the end of each stage, we found the total time of a single cycle is 90 minutes. So to get the better sleep you should manage your sleeping schedule according to these sleeping cycles.
+And Sleep Tracker tool will help you to figure it out.</p>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime neque id quasi nobis! Necessitatibus dicta voluptas itaque placeat, dolores sit natus voluptates quasi aut fuga doloremque ullam, omnis cupiditate maxime?</p>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime neque id quasi nobis! Necessitatibus dicta voluptas itaque placeat, dolores sit natus voluptates quasi aut fuga doloremque ullam, omnis cupiditate maxime?</p>
                     </div>            
